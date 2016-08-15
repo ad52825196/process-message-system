f68ff3f44ed235700c967e95d5b92a91074fb44b
Zhen Chen <me@zhen-chen.com>
2016-08-15 12:28:33 +1200

    Initialise MessageProc

--- process_message_system.py
+++ process_message_system.py
@@ -21,10 +21,19 @@ class TimeOut():
         self.action = action
 
 class MessageProc():
-    def main():
+    pipe_path_prefix = tempfile.gettempdir() + '/pipe'
+
+    def __init__(self):
+        self.arrived_condition = threading.Condition()
+        self.pipe_path = self.pipe_path_prefix + str(os.getpid())
+        self.pipes_written = {}
+        self.queue = queue.Queue()
+        self.message_list = []
+
+    def main(self):
         """Set up the communication mechanism"""
 
-    def start():
+    def start(self, *args):
         """
         Start up a new process and run the main() method in the new process
 
